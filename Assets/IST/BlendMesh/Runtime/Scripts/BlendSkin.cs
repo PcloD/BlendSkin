51edc1b1ab187fe700a72c83c13ef2e3175f82b9
i-saint <saint.skr@gmail.com>
2019-01-02 06:06:49 +0900

    wip

--- Assets/IST/BlendMesh/Runtime/Scripts/BlendSkin.cs
+++ Assets/IST/BlendMesh/Runtime/Scripts/BlendSkin.cs
@@ -13,11 +13,17 @@ namespace IST.BlendSkin
     {
         #region Fields
         public SkinnedMeshRenderer baseMesh;
-        public List<BlendSource> blendSources;
+        public List<BlendSource> blendSources = new List<BlendSource>();
+        public List<Material> materials = new List<Material>();
 
-        [SerializeField] ComputeShader blendCompute;
-        MeshBuffers baseMeshBuffers = new MeshBuffers();
-        List<MeshBuffers> blendSourceBuffers = new List<MeshBuffers>();
+        [SerializeField] ComputeShader m_blendCompute;
+        MeshBuffers m_baseMeshBuffers = new MeshBuffers();
+        List<MeshBuffers> m_blendSourceBuffers = new List<MeshBuffers>();
+
+        int m_kSkinning = -1;
+        int m_kBlendPrepare = -1;
+        int m_kBlendAdd = -1;
+        int m_kBlendFinish = -1;
         #endregion
 
 
@@ -26,7 +32,7 @@ namespace IST.BlendSkin
         public class BlendSource
         {
             public SkinnedMeshRenderer mesh;
-            public float weight;
+            [Range(-5, 5)] public float weight;
         }
 
         // non-serializable
@@ -40,6 +46,15 @@ namespace IST.BlendSkin
             public ComputeBuffer dstPoints;
             public ComputeBuffer dstNormals;
             public ComputeBuffer dstTangents;
+
+            public bool valid
+            {
+                get { return srcPoints != null; }
+            }
+            public bool isSkinned
+            {
+                get { return bones != null && weights != null; }
+            }
 
             public void Release()
             {
@@ -90,13 +105,25 @@ namespace IST.BlendSkin
             public void Assign(SkinnedMeshRenderer smr)
             {
                 Release();
+
                 if (smr == null)
                     return;
                 var mesh = smr.sharedMesh;
                 if (mesh == null)
                     return;
 
-                var b = smr.bones;
+                {
+                    var b = smr.bones;
+                    if (b.Length > 0)
+                    {
+                        var matrices = new Matrix4x4[b.Length];
+                        for (int bi = 0; bi < b.Length; ++bi)
+                            if (b[bi] != null)
+                                matrices[bi] = b[bi].localToWorldMatrix;
+                        bones = new ComputeBuffer(b.Length, 64);
+                        bones.SetData(matrices);
+                    }
+                }
 
                 var vertexCount = mesh.vertexCount;
                 {
@@ -138,37 +165,102 @@ namespace IST.BlendSkin
 
 
         #region Impl
-        void Release()
+        void ReleaseBuffers()
         {
-            baseMeshBuffers.Release();
-            foreach (var b in blendSourceBuffers)
+            m_baseMeshBuffers.Release();
+            foreach (var b in m_blendSourceBuffers)
                 b.Release();
-            blendSourceBuffers.Clear();
+            m_blendSourceBuffers.Clear();
+        }
+
+        void SetupResources()
+        {
+#if UNITY_EDITOR
+            if (m_blendCompute == null)
+                m_blendCompute = AssetDatabase.LoadAssetAtPath<ComputeShader>(AssetDatabase.GUIDToAssetPath("6c9bb1542a25b414d8eb9a86d401531f"));
+#endif
+        }
+
+        void SetupKernels()
+        {
+            if (m_kSkinning < 0)
+                m_kSkinning = m_blendCompute.FindKernel("Skinning");
+            if (m_kBlendPrepare < 0)
+                m_kBlendPrepare = m_blendCompute.FindKernel("BlendPrepare");
+            if (m_kBlendAdd < 0)
+                m_kBlendAdd = m_blendCompute.FindKernel("BlendAdd");
+            if (m_kBlendFinish < 0)
+                m_kBlendFinish = m_blendCompute.FindKernel("BlendFinish");
+        }
+
+        void SetupBuffers()
+        {
+            if (!m_baseMeshBuffers.valid)
+                m_baseMeshBuffers.Assign(baseMesh);
+
+            if (m_blendSourceBuffers.Count != blendSources.Count)
+            {
+                m_blendSourceBuffers.Clear();
+                for (int si = 0; si < blendSources.Count; ++si)
+                {
+                    var tmp = new MeshBuffers();
+                    tmp.Assign(blendSources[si].mesh);
+                    m_blendSourceBuffers.Add(tmp);
+                }
+            }
+        }
+
+        void DoSkinningAndBlending()
+        {
+            SetupKernels();
+            SetupBuffers();
+
+            // todo
+        }
+
+        void Draw()
+        {
+            // todo
         }
 
 #if UNITY_EDITOR
         void Reset()
         {
-            blendCompute = AssetDatabase.LoadAssetAtPath<ComputeShader>(AssetDatabase.GUIDToAssetPath("6c9bb1542a25b414d8eb9a86d401531f"));
+            ReleaseBuffers();
+            SetupResources();
         }
 
         void OnValidate()
         {
+            ReleaseBuffers();
+            SetupResources();
         }
 #endif
 
         void OnEnable()
         {
-
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying)
+                EditorApplication.update += Update;
+#endif
         }
 
         void OnDisable()
         {
-            Release();
+            ReleaseBuffers();
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying)
+                EditorApplication.update -= Update;
+#endif
         }
 
         void Update()
         {
+            if (baseMesh == null)
+                return;
+
+            DoSkinningAndBlending();
+            Draw();
         }
         #endregion
     }
--- Assets/IST/BlendMesh/Runtime/Shaders/BlendSkinStandard.shader
+++ Assets/IST/BlendMesh/Runtime/Shaders/BlendSkinStandard.shader
@@ -42,7 +42,6 @@
 #endif
         };
 
-
         struct Input
         {
             float2 uv_MainTex;
@@ -64,7 +63,7 @@
 #endif
         }
 
-        void surf (Input IN, inout SurfaceOutputStandard o)
+        void surf(Input IN, inout SurfaceOutputStandard o)
         {
             // Albedo comes from a texture tinted by color
             fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
